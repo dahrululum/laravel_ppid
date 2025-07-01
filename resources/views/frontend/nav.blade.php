46a1d7193631f8874bbae9a933e5c8b0be6ddcaa
dahrul <dahruloelum@gmail.com>
2025-07-01 14:15:11 +0700

    rev nav 01juli2025

--- resources/views/frontend/nav.blade.php
+++ resources/views/frontend/nav.blade.php
@@ -17,31 +17,68 @@
                 <li class="nav-item">
                     <a class="nav-link click-scroll" href="#top">Home</a>
                 </li>
-
-                <li class="nav-item">
-                    <a class="nav-link click-scroll" href="#section_2">About</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link click-scroll" href="#section_3">Causes</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link click-scroll" href="#section_4">Volunteer</a>
-                </li>
-
                 <li class="nav-item dropdown">
-                    <a class="nav-link click-scroll dropdown-toggle" href="#section_5" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">News</a>
+                    <a class="nav-link click-scroll dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Profil</a>
 
                     <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                        <li><a class="dropdown-item" href="news.html">News Listing</a></li>
+                        <li><a class="dropdown-item" href="#">Pemerintahan Provinsi Kepulauan Bangka Belitung</a></li>
 
-                        <li><a class="dropdown-item" href="news-detail.html">News Detail</a></li>
+                        <li><a class="dropdown-item" href="#">PPID Bangka Belitung</a></li>
                     </ul>
                 </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link click-scroll dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Informasi Publik</a>
+
+                    <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Daftar Informasi Publik</a></li>
+                        <li><a class="dropdown-item" href="#">Daftar Informasi Publik Online</a></li>
+                        <li><a class="dropdown-item" href="#">Informasi Berkala</a></li>
+                        <li><a class="dropdown-item" href="#">Informasi Serta Merta</a></li>
+                        <li><a class="dropdown-item" href="#">Informasi Setiap Saat</a></li>
+                        
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link click-scroll dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Standar Layanan</a>
+
+                    <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Prosedur Pelayanan Informasi Publik</a></li>
+
+                        <li><a class="dropdown-item" href="#">Prosedur Pengajuan Keberatan dan Proses Penyelesaian Sengketa Informasi</a></li>
+                        <li><a class="dropdown-item" href="#">Prosedur Permohonan Penyelesaian Sengketa Informasi</a></li>
+                        <li><a class="dropdown-item" href="#">SOP PPID</a></li>
+                        <li><a class="dropdown-item" href="#">Maklumat Informasi</a></li>
+                        <li><a class="dropdown-item" href="#">Waktu dan Biaya Layanan Informasi </a></li>
+                        <li><a class="dropdown-item" href="#">Kanal Layanan Informasi </a></li>
+                        
+                    </ul>
+                </li>
+                
 
                 <li class="nav-item">
-                    <a class="nav-link click-scroll" href="#section_6">Contact</a>
+                    <a class="nav-link click-scroll" href="#section_3">Berita</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link click-scroll dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Laporan</a>
+
+                    <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink2">
+                         <li class="dropend">
+                            <a class="dropdown-item dropdown-toggle" href="#" id="dropdownSubSubMenu1" data-bs-toggle="dropdown" aria-expanded="false">
+                                Laporan
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownSubSubMenu1">
+                                <li><a class="dropdown-item" href="#">Sub-Sub Item 1</a></li>
+                                <li><a class="dropdown-item" href="#">Sub-Sub Item 2</a></li>
+                            </ul>
+                        </li>
+
+                        <li><a class="dropdown-item" href="#">PPID </a></li>
+                    </ul>
+                </li>
+               
+
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="#section_6">Kontak Kami</a>
                 </li>
 
                 
